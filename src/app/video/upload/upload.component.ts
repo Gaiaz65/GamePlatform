1a2be57b486f39a66fc66c02289667efb623771a
Gaiaz65 <gayazapkaev365@gmail.com>
2022-09-19 12:59:41 +0300

    ckuo model has been created and defined. Video (clip) upload process has been defined ( clip service created)

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -1,40 +1,56 @@
-import { Component, OnInit } from '@angular/core';
-import { AngularFireStorage } from '@angular/fire/compat/storage';
+import { ClipService } from './../../services/clip.service';
+
+import { Component, OnDestroy } from '@angular/core';
+import { AngularFireStorage, AngularFireUploadTask } from '@angular/fire/compat/storage';
 import { FormControl,FormGroup,Validators } from '@angular/forms';
+import { last, switchMap } from 'rxjs';
 import { v4 as uuid } from 'uuid';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import firebase from 'firebase/compat/app';
 
 @Component({
   selector: 'app-upload',
   templateUrl: './upload.component.html',
   styleUrls: ['./upload.component.scss'],
 })
-export class UploadComponent implements OnInit {
-  isDragover = false
-  file: File | null = null
-  loadingVideo = false
+export class UploadComponent implements OnDestroy {
+  isDragover = false;
+  file: File | null = null;
+  user: firebase.User | null = null;
+  loadingVideo = false;
+  showPercentage = false;
 
-  showAlert = false
-  alertColor = 'blue'
-  alertMsg = 'Processing the uploading'
-  inSubmission = false
-  progress = 0
+  showAlert = false;
+  alertColor = 'blue';
+  alertMsg = 'Processing the uploading';
+  inSubmission = false;
+  progress = 0;
+  task?: AngularFireUploadTask
 
   title = new FormControl('', {
     validators: [Validators.required, Validators.minLength(3)],
   });
-  upluadFrom = new FormGroup({
+  uploadFormular = new FormGroup({
     title: this.title,
   });
 
   constructor(
-    private storage: AngularFireStorage) {}
+    private storage: AngularFireStorage,
+    private auth: AngularFireAuth,
+    private clipService: ClipService
+  ) {
+    auth.user.subscribe((user) => (this.user = user));
+  }
 
-  ngOnInit(): void {}
+  ngOnDestroy(): void {
+  this.task?.cancel()
+  }
 
   storeFile(event: Event) {
     this.isDragover = false;
-    this.file = (event as DragEvent).dataTransfer?.files.item(0) ?? null;
-    console.log(this.file);
+    this.file = (event as DragEvent).dataTransfer
+      ? (event as DragEvent).dataTransfer?.files.item(0) ?? null
+      : (event.target as HTMLInputElement).files?.item(0) ?? null;
 
     if (!this.file || this.file.type !== 'video/mp4') {
       return;
@@ -44,18 +60,55 @@ export class UploadComponent implements OnInit {
   }
 
   uploadFile() {
-      this.showAlert = true;
-      this.alertColor = 'blue';
-      this.alertMsg = 'Processing the uploading';
-      this.inSubmission = true;
+    this.uploadFormular.disable();
 
+    this.showAlert = true;
+    this.alertColor = 'blue';
+    this.alertMsg = 'Processing the uploading';
+    this.inSubmission = true;
+    this.showPercentage = true;
 
-    const clipFileName = uuid()
-    console.log(clipFileName);
+    const clipFileName = uuid();
     const clipPath = `clips/${clipFileName}.mp4`;
-    const task = this.storage.upload(clipPath, this.file)
-    task.percentageChanges().subscribe( progress => {
-      this.progress = progress as number / 100
-    })
+
+    this.task = this.storage.upload(clipPath, this.file);
+    const clipRef = this.storage.ref(clipPath);
+
+    this.task.percentageChanges().subscribe((progress) => {
+      this.progress = (progress as number) / 100;
+    });
+
+    this.task
+      .snapshotChanges()
+      .pipe(
+        last(),
+        switchMap(() => clipRef.getDownloadURL())
+      )
+      .subscribe({
+        next: (url) => {
+          const clip = {
+            uid: this.user?.uid as string,
+            displayName: this.user?.displayName as string,
+            title: this.title.value,
+            fileName: `clips/${clipFileName}.mp4`,
+            url,
+          };
+
+          this.clipService.createClip(clip);
+
+          this.alertColor = 'green';
+          this.alertMsg = 'The video has been uploaded';
+          this.showPercentage = false;
+        },
+        error: (error) => {
+          this.uploadFormular.enable();
+
+          this.alertColor = 'red';
+          this.alertMsg = 'Something went wrong, try again later';
+          this.showPercentage = false;
+          this.inSubmission = true;
+          console.error(error);
+        },
+      });
   }
 }
